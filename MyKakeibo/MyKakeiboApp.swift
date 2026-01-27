d7d41c7562f00be03dee0a55d554f87665cc3a0a
Yuichiro <yuicya1133@gmail.com>
2026-01-27 12:03:15 +0900

    Initial Commit

---
+++ MyKakeibo/MyKakeiboApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyKakeiboApp.swift
+//  MyKakeibo
+//
+//  Created by Yuichiro Yano on 2026/01/27.
+//
+
+import SwiftUI
+
+@main
+struct MyKakeiboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyKakeiboTests/MyKakeiboTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyKakeiboTests.swift
+//  MyKakeiboTests
+//
+//  Created by Yuichiro Yano on 2026/01/27.
+//
+
+import Testing
+@testable import MyKakeibo
+
+struct MyKakeiboTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
